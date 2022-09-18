aa1b1447d67e37d7c2e07f2b510357bf4178a8a6
Rafael Shakhnazarian <rshakhnazari@adobe.com>
2022-09-18 19:14:27 +0400

    profile fragment added

--- app/src/main/java/com/example/navio/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/navio/ui/account/AccountFragment.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.auth0.android.jwt.JWT;
 import com.example.navio.R;
 import com.example.navio.backend.local_storage.LocalStorage;
 import com.example.navio.databinding.FragmentAccountBinding;
@@ -31,6 +33,25 @@ public class AccountFragment extends Fragment {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         final View root = binding.getRoot();
         final Button logoutButton = root.findViewById(R.id.logout_button);
+        final TextView name = root.findViewById(R.id.name);
+        final TextView surname = root.findViewById(R.id.surname);
+        final TextView username = root.findViewById(R.id.username);
+        final TextView departmentId = root.findViewById(R.id.department_id);
+        final TextView email = root.findViewById(R.id.email);
+
+        final String jwtToken = LocalStorage.getInstance()
+                .setContext(root.getContext())
+                .setKey(getString(R.string.local_authentication))
+                .getString(getString(R.string.local_jwt));
+
+        if (jwtToken != null) {
+            final JWT jwt = new JWT(jwtToken);
+            name.setText(jwt.getClaim("name").asString());
+            surname.setText(jwt.getClaim("surname").asString());
+            username.setText(jwt.getClaim("username").asString());
+            departmentId.setText(String.format("Department ID:  %s", jwt.getClaim("department_id").asString()));
+            email.setText(jwt.getClaim("email").asString());
+        }
 
         logoutButton.setOnClickListener(v -> {
             LocalStorage.getInstance()
