552cdaf0b7e53c5624e986060e4a1b56f6fb6f89
Rafael Shakhnazarian <rshakhnazari@adobe.com>
2022-09-21 17:18:04 +0400

    one card design

--- app/src/main/java/com/example/navio/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/navio/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.navio.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +8,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.auth0.android.jwt.JWT;
 import com.example.navio.R;
 import com.example.navio.backend.local_storage.LocalStorage;
 import com.example.navio.databinding.FragmentHomeBinding;
+import com.example.navio.ui.home.cards.PendingTasksViewMoreActivity;
+import com.example.navio.ui.splash.SplashActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -24,6 +28,7 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView nameTextView = root.findViewById(R.id.name);
+        final ConstraintLayout pendingTasksViewMoreLayout = root.findViewById(R.id.pending_tasks_view_more_layout);
 
         final String jwtToken = LocalStorage.getInstance()
                 .setContext(root.getContext())
@@ -35,6 +40,11 @@ public class HomeFragment extends Fragment {
             final String name = jwt.getClaim("name").asString();
             nameTextView.setText(name);
         }
+
+        pendingTasksViewMoreLayout.setOnClickListener(v -> {
+            final Intent intent = new Intent(root.getContext(), PendingTasksViewMoreActivity.class);
+            startActivity(intent);
+        });
 
         return root;
     }
